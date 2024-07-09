ba549a20713abd57697a3f7b55516b14083a1b59
Robert Montgomery <boobyvxvbooby_gmail.com>
2024-07-08 21:45:55 -0500

    Created a simple character creation

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,53 @@
-﻿namespace VariablesExercise
+﻿using System.Net.NetworkInformation;
+
+namespace VariablesExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            //USER WILL ENTER THE NAME OF THEIR CHARACTER
+            Console.WriteLine("Give me your name Hero:");
+
+            string charName = Console.ReadLine();
+                
+                Console.WriteLine($"Welcome,{charName} to your doom!");
+                
+                //Current lvl of the new character will always be 1
+                int lvlNumber = 1;
+
+                Console.WriteLine($"Currently level 1");
+                
+                //New user will always start at 100 health
+                double currentHealth = 100.00;
+                
+                Console.WriteLine($"Current Health 100");
+                
+                //Is the current character alive? yes if value of health still = positive
+                bool charAlive = true;
+                
+                //Character current stats Strength, Dexterity, Intelligence, Wisdom, Constitution, Charisma(use H) X is variable stat 
+                char stats = 'x';
+                
+                Console.WriteLine($"Strength: 10");
+                Console.WriteLine($"Dexterity: 12");
+                Console.WriteLine($"Intelligence: 2");
+                Console.WriteLine($"Wisdom: 1");
+                Console.WriteLine($"Constitution: 1");
+                Console.WriteLine($"Charisma: 99");
+
+                //New user will always start with 10 copper to their name!
+                decimal myWallet = 10.00m;
+                
+                Console.WriteLine($"Currency: 10 Copper in purse you are poor! not enough to even afford scraps!");
+                
+                
+                
+                
+
+
+
         }
     }
 }
